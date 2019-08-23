acc18c86a854470770c39c892f76fb14aa66d3a4
Shiliang0352 <1161943164@qq.com>
2019-08-23 18:39:26 +0800

    6

--- resources/views/travel/resource/edit.blade.php
+++ resources/views/travel/resource/edit.blade.php
@@ -1,3 +1,9 @@
+<script type="text/javascript" charset="utf-8" src="/Uedit/ueditor.config.js"></script>
+<script type="text/javascript" charset="utf-8" src="/Uedit/ueditor.all.min.js"> </script>
+<!--建议手动加在语言，避免在ie下有时因为加载语言失败导致编辑器加载失败-->
+<!--这里加载的语言文件会覆盖你在配置项目里添加的语言类型，比如你在配置项目里配置的是英文，这里加载的中文，那最后就是中文-->
+<script type="text/javascript" charset="utf-8" src="/Uedit/lang/zh-cn/zh-cn.js"></script>
+
 @extends('travel.layouts.index')
 
 @section('style')
@@ -282,10 +288,7 @@
         </form>
     </div>
 
-    <!-- 配置文件 -->
-    <script type="text/javascript" src="/ue/ueditor.config.js"></script>
-    <!-- 编辑器源码文件 -->
-    <script type="text/javascript" src="/ue/ueditor.all.js"></script>
+
 @endsection
 @section('script')
     //实例化编辑器
